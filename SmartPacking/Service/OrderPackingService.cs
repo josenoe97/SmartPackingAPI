5b04f3af2eb0e9e981c16a342b2ca55f7f06eb4c
Junior <edsonjunior9701@outlook.com>
2025-05-30 02:04:12 -0300

    refact: reducao de codigo em metodo de ProcessOrders

--- SmartPacking/Service/OrderPackingService.cs
+++ SmartPacking/Service/OrderPackingService.cs
@@ -1,6 +1,7 @@
 ﻿using SmartPacking.Data.Dto;
 using SmartPacking.Model;
 using SmartPacking.Service.Interfaces;
+using System.Diagnostics.Eventing.Reader;
 
 namespace SmartPacking.Service
 {
@@ -17,51 +18,27 @@ namespace SmartPacking.Service
 
                 var orderVolumeTotal = OrderVolumeTotalCalculator(order);
 
+                var box = new BoxModel();
+
                 if (orderVolumeTotal <= boxList[0].Volume)
+                    box = boxList[0];
+                else if (orderVolumeTotal <= boxList[1].Volume)
+                    box = boxList[1];
+                else if (orderVolumeTotal <= boxList[2].Volume)
+                    box = boxList[3];
+
+                var boxResult = new BoxResultDto() { NameBox = box.Name };
+
+                boxResult.Products = new List<ProductBoxResultDto>();
+
+                foreach (var product in order.listProduct)
                 {
-                    var boxResult = new BoxResultDto() { NameBox = boxList[0].Name };
+                    var productBoxResult = new ProductBoxResultDto() { Name = product.Name, ProductId = product.Id };
 
-                    boxResult.Products = new List<ProductBoxResultDto>();
-
-                    foreach (var product in order.listProduct)
-                    {
-                        var productBoxResult = new ProductBoxResultDto() { Name = product.Name, ProductId = product.Id };
-
-                        boxResult.Products.Add(productBoxResult);
-                    }
-
-                    orderPackingResult.Boxes.Add(boxResult);
+                    boxResult.Products.Add(productBoxResult);
                 }
-                else if(orderVolumeTotal <= boxList[1].Volume)
-                {
-                    var boxResult = new BoxResultDto() { NameBox = boxList[0].Name };
 
-                    boxResult.Products = new List<ProductBoxResultDto>();
-
-                    foreach (var product in order.listProduct)
-                    {
-                        var productBoxResult = new ProductBoxResultDto() { Name = product.Name, ProductId = product.Id };
-
-                        boxResult.Products.Add(productBoxResult);
-                    }
-
-                    orderPackingResult.Boxes.Add(boxResult);
-                }
-                else if(orderVolumeTotal <= boxList[2].Volume)
-                {
-                    var boxResult = new BoxResultDto() { NameBox = boxList[0].Name };
-
-                    boxResult.Products = new List<ProductBoxResultDto>();
-
-                    foreach (var product in order.listProduct)
-                    {
-                        var productBoxResult = new ProductBoxResultDto() { Name = product.Name, ProductId = product.Id };
-
-                        boxResult.Products.Add(productBoxResult);
-                    }
-
-                    orderPackingResult.Boxes.Add(boxResult);
-                }
+                orderPackingResult.Boxes.Add(boxResult);
 
                 result.Add(orderPackingResult);
             }
